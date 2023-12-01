2524287df935e456319952864fe590fe44cee2f8
Md. Atikur Rahman <atik.rpi27@gmail.com>
2023-12-02 00:28:15 +0600

    add sidebar with tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
+import Sidebar from './Components/sidebar'
 
 function App() {
 
   return (
-    <div className='text-center'>
-      <h2 className='text-4xl bold font-bold'>Welcome to</h2> Project Management App
-    </div>
+    <main className='h-screen my-8'>
+      <Sidebar />
+    </main>
   )
 }
 
---
+++ src/Components/sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function sdfdaf() {
+  return (
+    <div className='w-1/3 h-full px-8 pt-10 bg-stone-900 text-stone-50 md:w-1/5 rounded-r-xl'>
+        <h2 className='mb-8 font-bold uppercase md:text-xl text-stone-200'>Your Project</h2>
+        <ul></ul>
+        <button className='px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100'>+ Add Project</button>
+    </div>
+  )
+}
