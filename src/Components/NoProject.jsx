210ef02c1cc4084bed64bd82dcf30036bc7fe897
Md. Atikur Rahman <58600348+atikrpi27@users.noreply.github.com>
2023-12-26 09:45:13 +0000

    no project page update

--- src/Components/NoProject.jsx
+++ src/Components/NoProject.jsx
@@ -4,7 +4,7 @@ import Button from './Button'
 
 export default function NoProject({ onHandleProject }) {
   return (
-    <div className='w-3/4 text-center flex flex-col h-screen justify-center items-center'>
+    <div className='w-full text-center flex flex-col h-screen justify-center items-center'>
       <img src={No_Project} alt="No Project Selected" className='h-28 w-28 mx-auto' />
       <p className='mt-4 mb-1 text-xl font-bold text-stone-700'>No Project Selected</p>
       <p className='mb-1'>Select a project or create a new one!</p>
